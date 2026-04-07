f359354565c1a5ff79590cc6efa34b0034290864
Thanakit2023 <150004716+thanakittt@users.noreply.github.com>
2026-04-07 17:35:46 +0700

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const requiredEnv = (key: string) => {
-  const value = process.env[key];
-  if (!value) throw new Error(`ไม่ได้ตั้งค่า env: ${key}`);
-  return value;
-};
