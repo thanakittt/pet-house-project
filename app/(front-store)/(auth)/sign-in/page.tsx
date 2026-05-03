f2fa03cb34ffa9e93b4939bba25089b759a62df3
Thanakit2023 <150004716+thanakittt@users.noreply.github.com>
2026-05-03 21:39:19 +0700

    Revert "Redirect authenticated users away from auth pages"
    This reverts commit ed66bccce257a44765ac0958f62a71d311782806.

--- app/(front-store)/(auth)/sign-in/page.tsx
+++ app/(front-store)/(auth)/sign-in/page.tsx
@@ -1,11 +1,5 @@
-import { requireCustomer } from "@/lib/session";
 import { SignInForm } from "@/modules/auth/components/SignInForm";
-import { redirect } from "next/navigation";
 
-export default async function SignIn() {
-  const session = await requireCustomer({ redirect: false });
-  if (session) {
-    redirect("/");
-  }
+export default function SignIn() {
   return <SignInForm />;
 }
--- app/(front-store)/(auth)/sign-up/page.tsx
+++ app/(front-store)/(auth)/sign-up/page.tsx
@@ -1,11 +1,5 @@
-import { requireCustomer } from "@/lib/session";
 import { SignUpForm } from "@/modules/auth/components/SignUpForm";
-import { redirect } from "next/navigation";
 
-export default async function SignUpPage() {
-  const session = await requireCustomer({ redirect: false });
-  if (session) {
-    redirect("/");
-  }
+export default function SignUpPage() {
   return <SignUpForm />;
 }
