5ad7e2a93d9910499e86cd84ce7821372ce0f482
Thanakit2023 <150004716+thanakittt@users.noreply.github.com>
2026-05-12 17:22:55 +0700

    Remove debug outline from back office container

--- components/shared/BackOfficeContainer.tsx
+++ components/shared/BackOfficeContainer.tsx
@@ -11,7 +11,7 @@ export function BackOfficeContainer({
   return (
     <div
       className={cn(
-        "flex flex-col mx-auto px-4 sm:px-6 lg:px-8 py-8 outline outline-red-500 w-full max-w-7xl",
+        "flex flex-col mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full max-w-7xl",
         className,
       )}
       {...props}
