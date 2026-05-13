3f9432fb51992b7352ea0c228ecf0a87da2231a3
bamboo513 <bamboobg14@gmail.com>
2026-05-13 18:53:45 +0700

    feat: implement reusable Table component with sub-components

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -70,7 +70,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-6 py-4 text-left align-middle bg-primary/10 font-semibold whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
+        "h-10 px-6 py-4 text-left align-middle bg-primary/5 font-semibold whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
