a2582989cd2b9e8035fd6a0b8baaa9fe387f38b2
bamboo513 <bamboobg14@gmail.com>
2026-05-13 19:10:02 +0700

    feat: add CustomerChannelBadge component to shared UI library

--- components/shared/CustomerChannelBadge.tsx
+++ components/shared/CustomerChannelBadge.tsx
@@ -15,12 +15,12 @@ const customerChannelConfig: Record<
   ONLINE: {
     label: "Online",
     className:
-      "border-blue-200 bg-blue-50 text-blue-700 dark:border-blue-900/50 dark:bg-blue-900/30 dark:text-blue-300",
+      "border-green-200 bg-green-50 text-green-700 dark:border-green-900/50 dark:bg-green-900/30 dark:text-green-300",
   },
   WALK_IN: {
     label: "Walk-in",
     className:
-      "border-amber-200 bg-amber-50 text-amber-700 dark:border-amber-900/50 dark:bg-amber-900/30 dark:text-amber-300",
+      "border-blue-200 bg-blue-50 text-blue-700 dark:border-blue-900/50 dark:bg-blue-900/30 dark:text-blue-300",
   },
 };
 
